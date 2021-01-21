cd276652e4b1bb8c0a55a20e03032e9e26d914d4
Segi H <shovav@cryptolab.net>
2021-01-20 21:23:42 -0800

    Update packages per ng update

--- src/app/wtfindexer/wtfindexer.component.spec.ts
+++ src/app/wtfindexer/wtfindexer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WTFIndexerComponent } from './wtfindexer.component';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +12,7 @@ import { CoreModule } from '../core/core.modules';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('WTFIndexerComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         BrowserAnimationsModule,
